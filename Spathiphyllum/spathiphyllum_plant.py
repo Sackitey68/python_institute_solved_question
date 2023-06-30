4a3b738a5536a2437aae5cbcc9364e9867020223
Sackitey68 <sackitey68@gmail.com>
2023-06-30 18:57:15 +0000

    Question solved

---
+++ Spathiphyllum/spathiphyllum_plant.py
@@ -0,0 +1,10 @@
+plant = input("Enter the name of the plant: ")
+
+if (plant == "Spathiphyllum"):
+    print("Yes - Spathiphyllum is the best plant ever!")
+            
+elif (plant == "spathiphyllum"):
+    print("No, I want a big Spathiphyllum!")
+                
+else:
+    print("Spathiphyllum! Not", plant + "!")
