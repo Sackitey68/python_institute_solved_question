b08e6f4adc1b63e576a9ccca996cbf96dd2358dc
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-02 12:41:02 +0000

    Update secret_number.py

--- guess_number/secret_number.py
+++ guess_number/secret_number.py
@@ -1,25 +1,23 @@
 secret_number = 777
 
 print(
-        """
-        +================================+
-        | Welcome to my game, muggle!    |
-        | Enter an integer number        |
-        | and guess what number I've     |
-        | picked for you.                |
-        | So, what is the secret number? |
-        +================================+
-        """)
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
 
 guess_number = int(input("Enter an integer number: "))
 
 while guess_number != secret_number:
-            print("Ha ha! You're stuck in my loop!")
+        print("Ha ha! You're stuck in my loop!")
 
-                    # Taking another number from the user
-                            guess_number = int(input("Enter an integer number: "))
+        # Taking another number from the user
+        guess_number = int(input("Enter an integer number: "))
 
-                            print(secret_number)
-                            print("Well done, muggle! You are free now.")
-
-                            
+print(secret_number)
+print("Well done, muggle! You are free now.")
