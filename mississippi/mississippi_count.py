9c7fd46680b67c0d7c9008ac65d58646df2ab6cb
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-02 15:29:43 +0000

    Update mississippi_count.py

--- mississippi/mississippi_count.py
+++ mississippi/mississippi_count.py
@@ -2,14 +2,15 @@ import time
 
 # Write a for loop that counts to five.
     # Body of the loop - print the loop iteration number and the word "Mississippi".
-        # Body of the loop - use: time.sleep(1)
+    # Body of the loop - use: time.sleep(1)
 
-        # Write a print function with the final message.
+# Write a print function with the final message.
 
-        for number in range(1, 6):
-                print(number, "Mississippi")
-                    #pause one second after each iteration
-                        time.sleep(1)
-                            
-                            print("Ready or not, here I come!")
+for number in range(1, 6):
+    print(number, "Mississippi")
+    #pause one second after each iteration
+    time.sleep(1)
+    
+print("Ready or not, here I come!")
+
 
