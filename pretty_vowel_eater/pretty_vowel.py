08facefba7a74a40851053ce9f3fcd7249740664
Sackitey68 <sackitey68@gmail.com>
2023-07-05 15:15:29 +0000

    Question solved

---
+++ pretty_vowel_eater/pretty_vowel.py
@@ -0,0 +1,31 @@
+word_without_vowels = ""
+
+# Prompt the user to enter a word
+user_word = input("Enter a word: ")
+user_word = user_word.upper()
+# and assign it to the user_word variable.
+
+
+for letter in user_word:
+        # Complete the body of the loop.
+
+    if letter == 'A':
+        continue
+                                
+    elif letter == 'E':
+        continue
+                                            
+    elif letter == 'I':
+        continue
+                                                
+    elif letter == 'O':
+        continue
+                                                                    
+    elif letter == 'U':
+        continue
+                                                                                
+    else:
+        word_without_vowels = letter
+
+# Print the word assigned to word_without_vowels.
+        print(word_without_vowels, end="")
