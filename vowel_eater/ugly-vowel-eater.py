cf6315feaa96adfc3e41619cc64e4df9c5f02dd4
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-05 14:45:52 +0000

    Update ugly-vowel-eater.py

--- vowel_eater/ugly-vowel-eater.py
+++ vowel_eater/ugly-vowel-eater.py
@@ -1,46 +1,24 @@
-er to enter a word
+# Prompt the user to enter a word
 user_word = input("Enter a word: ")
 user_word = user_word.upper()
 # and assign it to the user_word variable.
 
 for letter in user_word:
-        # Complete the body of the for loop.
-            if letter == 'A':
-                        continue
-                        
-                        elif letter == 'E':
-                                    continue
-                                    
-                                    elif letter == 'I':
-                                                continue
-                                                
-                                                elif letter == 'O':
-                                                            continue
-                                                            
-                                                            elif letter == 'U':
-                                                                        continue
-                                                                        
-                                                                        else:
-                                                                                    print(letter)# Prompt the user to enter a word
-                                                                                    user_word = input("Enter a word: ")
-                                                                                    user_word = user_word.upper()
-                                                                                    # and assign it to the user_word variable.
-
-                                                                                    for letter in user_word:
-                                                                                            # Complete the body of the for loop.
-                                                                                                if letter == 'A':
-                                                                                                            continue
-                                                                                                            
-                                                                                                            elif letter == 'E':
-                                                                                                                        continue
-                                                                                                                        
-                                                                                                                        elif letter == 'I':
-                                                                                                                                    continue
-                                                                                                                                    
-                                                                                                                                    elif letter == 'O':
-                                                                                                                                                continue
-                                                                                                                                                
-                                                                                                                                                elif letter == 'U':
-                                                                                                                                                            continue
-                                                                                                                                                            
-                                                                                                                                                                                    print(letter)
+    # Complete the body of the for loop.
+    if letter == 'A':
+        continue
+    
+    elif letter == 'E':
+        continue
+    
+    elif letter == 'I':
+        continue
+    
+    elif letter == 'O':
+        continue
+    
+    elif letter == 'U':
+        continue
+    
+    else:
+        print(letter)
