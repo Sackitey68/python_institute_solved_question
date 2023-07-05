2647602e2d655743adfe2851fe2aa4ea8e479093
Sackitey68 <sackitey68@gmail.com>
2023-07-05 14:19:34 +0000

    Question solved

---
+++ break_keyword/while.py
@@ -0,0 +1,10 @@
+word = input("Enter a word: ")
+
+
+while True:
+    if word == "chupacabra":
+        print("You've successfully left the loop.")
+        break
+
+    else:
+        word = input("Enter a word: ")            
