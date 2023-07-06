fe95e4f0373a97f396245b5384a532900b977712
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-06 02:01:14 +0000

    Update collatz-s-hypothesis.py

--- lothar_collatz/collatz-s-hypothesis.py
+++ lothar_collatz/collatz-s-hypothesis.py
@@ -1,33 +1,17 @@
 c0 = int(input("Enter any non-negative and non-zero integer number: "))
 if c0 < 1:
-        print("The value but be a non-negative or non-zero integer number")
+    print("The value but be a non-negative or non-zero integer number")
 
-    else:
+else:
 
-            steps = 0
-                while c0 != 1:
-                            if c0 % 2 == 0:
-                                            c0 = c0 // 2
+    steps = 0
+    while c0 != 1:
+        if c0 % 2 == 0:
+            c0 = c0 // 2
 
-                                                    else:
-                                                                    c0 = 3 * c0 + 1
-                                                                            print(c0)
-                                                                                    steps = steps + 1
+        else:
+            c0 = 3 * c0 + 1
+        print(c0)
+        steps = steps + 1
 
-                                                                                        print("steps = ", steps)c0 = int(input("Enter any non-negative and non-zero integer number: "))
-                                                                                        if c0 < 1:
-                                                                                                print("The value but be a non-negative or non-zero integer number")
-
-                                                                                            else:
-
-                                                                                                    steps = 0
-                                                                                                        while c0 != 1:
-                                                                                                                    if c0 % 2 == 0:
-                                                                                                                                    c0 = c0 // 2
-
-                                                                                                                                            else:
-                                                                                                                                                            c0 = 3 * c0 + 1
-                                                                                                                                                                    print(c0)
-                                                                                                                                                                            steps = steps + 1
-
-                                                                                                                                                                                print("steps = ", steps)
+    print("steps = ", steps)
