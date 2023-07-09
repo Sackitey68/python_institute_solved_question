c53e18c811dc9f533a64dbbfea2872c2e4472a71
Sackitey68 <sackitey68@gmail.com>
2023-07-09 03:08:22 +0000

    Question solved

---
+++ remove_list_duplicate/list_duplicate.py
@@ -0,0 +1,9 @@
+ult = []
+for item in my_list:
+        if item not in result:
+                    result.append(item)
+                    #
+
+                    my_list = result
+                    print("The list with unique elements only:")
+                    print(my_list)
