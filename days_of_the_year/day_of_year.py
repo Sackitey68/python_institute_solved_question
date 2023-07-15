5ad674f19df8c5747e49f568ea485cb466941953
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-15 16:52:14 +0000

    Update day_of_year.py

--- days_of_the_year/day_of_year.py
+++ days_of_the_year/day_of_year.py
@@ -1,93 +1,46 @@
 def is_year_leap(year):
 
-        if (year < 1582):
-                    return False 
+    if (year < 1582):
+        return False 
 
-                    elif (year % 4 != 0):
-                                return False
+    elif (year % 4 != 0):
+        return False
 
-                                elif (year % 100 != 0):
-                                            return True
-                                                            
-                                            elif (year % 400 != 0):
-                                                        return False    
-                                                        
-                                                        else:
-                                                                    return True
+    elif (year % 100 != 0):
+        return True
+                    
+    elif (year % 400 != 0):
+        return False    
+    
+    else:
+        return True
 
-                                                                def days_in_month(year, month):
-                                                                        monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-                                                                            if is_year_leap(year) and month == 2:
-                                                                                        return 29
-                                                                                            
-                                                                                        return monthDays[month - 1]
+def days_in_month(year, month):
+    monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_year_leap(year) and month == 2:
+        return 29
+        
+    return monthDays[month - 1]
 
-                                                                                    def day_of_year(year, month, day):
-                                                                                        # Input validation
+def day_of_year(year, month, day):
+# Input validation
 
-                                                                                            if year < 1582:
-                                                                                                        return None
-                                                                                                            
-                                                                                                        if month < 1 or month > 12:
-                                                                                                                    return None
-                                                                                                                    
-                                                                                                                    if day > days_in_month(year, month) or day < 1:
-                                                                                                                                return None
-                                                                                                                                
-                                                                                                                                #calculate days
-                                                                                                                                    totalDays = day
-                                                                                                                                        month = month - 1
-                                                                                                                                            while month > 0:
-                                                                                                                                                        totalDays += days_in_month(year, month)
-                                                                                                                                                                month -= 1
+    if year < 1582:
+        return None
+        
+    if month < 1 or month > 12:
+        return None
+    
+    if day > days_in_month(year, month) or day < 1:
+        return None
+    
+    #calculate days
+    totalDays = day
+    month = month - 1
+    while month > 0:
+        totalDays += days_in_month(year, month)
+        month -= 1
 
-                                                                                                                                                                    return totalDays
+    return totalDays
 
-                                                                                                                                                                print(day_of_year(2001, 2, 29))
-                                                                                                                                                                def is_year_leap(year):
-
-                                                                                                                                                                        if (year < 1582):
-                                                                                                                                                                                    return False 
-
-                                                                                                                                                                                    elif (year % 4 != 0):
-                                                                                                                                                                                                return False
-
-                                                                                                                                                                                                elif (year % 100 != 0):
-                                                                                                                                                                                                            return True
-                                                                                                                                                                                                                            
-                                                                                                                                                                                                            elif (year % 400 != 0):
-                                                                                                                                                                                                                        return False    
-                                                                                                                                                                                                                        
-                                                                                                                                                                                                                        else:
-                                                                                                                                                                                                                                    return True
-
-                                                                                                                                                                                                                                def days_in_month(year, month):
-                                                                                                                                                                                                                                        monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-                                                                                                                                                                                                                                            if is_year_leap(year) and month == 2:
-                                                                                                                                                                                                                                                        return 29
-                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                        return monthDays[month - 1]
-
-                                                                                                                                                                                                                                                    def day_of_year(year, month, day):
-                                                                                                                                                                                                                                                        # Input validation
-
-                                                                                                                                                                                                                                                            if year < 1582:
-                                                                                                                                                                                                                                                                        return None
-                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                        if month < 1 or month > 12:
-                                                                                                                                                                                                                                                                                    return None
-                                                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                                    if day > days_in_month(year, month) or day < 1:
-                                                                                                                                                                                                                                                                                                return None
-                                                                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                                #calculate days
-                                                                                                                                                                                                                                                                                                    totalDays = day
-                                                                                                                                                                                                                                                                                                        month = month - 1
-                                                                                                                                                                                                                                                                                                            while month > 0:
-                                                                                                                                                                                                                                                                                                                        totalDays += days_in_month(year, month)
-                                                                                                                                                                                                                                                                                                                                month -= 1
-
-                                                                                                                                                                                                                                                                                                                                    return totalDays
-
-                                                                                                                                                                                                                                                                                                                                print(day_of_year(2001, 2, 29))
-
+print(day_of_year(2001, 2, 29))
