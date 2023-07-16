e7f617e2b4aa4d9a893372dfcbd57d8bea161f8d
Sackitey68 <sackitey68@gmail.com>
2023-07-16 12:02:47 +0000

    Question solved

---
+++ converting_fuel_consumption/README.md
@@ -0,0 +1,30 @@
+objectives
+	improving the student's skills in defining, using and testing functions.
+
+Scenario
+A car's fuel consumption may be expressed in many different ways. For example, in Europe, it is shown as the amount of fuel consumed per 100 kilometers.
+
+In the USA, it is shown as the number of miles traveled by a car using one gallon of fuel.
+
+Your task is to write a pair of functions converting l/100km into mpg, and vice versa.
+
+The functions:
+
+are named liters_100km_to_miles_gallon and miles_gallon_to_liters_100km respectively;
+take one argument (the value corresponding to their names)
+Complete the code in the editor.
+
+Run your code and check whether your output is the same as ours.
+
+Here is some information to help you:
+
+1 American mile = 1609.344 metres;
+1 American gallon = 3.785411784 litres.
+
+Expected output
+60.31143162393162
+31.36194444444444
+23.52145833333333
+3.9007393587617467
+7.490910297239916
+10.009131205673757
---
+++ converting_fuel_consumption/fuel_consumption.py
@@ -0,0 +1,41 @@
+def liters_100km_to_miles_gallon(liters):
+        gallons = liters / 3.785411784 
+            # 1 mile = 1.60934 meters
+                # 1 km   = 1000 meters .. 1000 * 1.60934 = 1609.344
+                    # 100km = 100 * 1000 meters 
+                        miles = 100 * 1000 / 1609.344
+                            return miles / gallons
+
+                        def miles_gallon_to_liters_100km(miles):
+                                kilometers = miles * 1609.344 / 1000
+                                    km_100 = kilometers / 100
+                                        liters = 3.785411784
+                                            return liters / km_100
+
+                                        print(liters_100km_to_miles_gallon(3.9))
+                                        print(liters_100km_to_miles_gallon(7.5))
+                                        print(liters_100km_to_miles_gallon(10.))
+                                        print(miles_gallon_to_liters_100km(60.3))
+                                        print(miles_gallon_to_liters_100km(31.4))
+                                        print(miles_gallon_to_liters_100km(23.5))
+                                        def liters_100km_to_miles_gallon(liters):
+                                                gallons = liters / 3.785411784 
+                                                    # 1 mile = 1.60934 meters
+                                                        # 1 km   = 1000 meters .. 1000 * 1.60934 = 1609.344
+                                                            # 100km = 100 * 1000 meters 
+                                                                miles = 100 * 1000 / 1609.344
+                                                                    return miles / gallons
+
+                                                                def miles_gallon_to_liters_100km(miles):
+                                                                        kilometers = miles * 1609.344 / 1000
+                                                                            km_100 = kilometers / 100
+                                                                                liters = 3.785411784
+                                                                                    return liters / km_100
+
+                                                                                print(liters_100km_to_miles_gallon(3.9))
+                                                                                print(liters_100km_to_miles_gallon(7.5))
+                                                                                print(liters_100km_to_miles_gallon(10.))
+                                                                                print(miles_gallon_to_liters_100km(60.3))
+                                                                                print(miles_gallon_to_liters_100km(31.4))
+                                                                                print(miles_gallon_to_liters_100km(23.5))
+
