d8361e60ddb7eb6d8627a4792d9adea45c557d4c
Isaac Sackitey <sackitey68@yahoo.com>
2023-07-16 11:22:08 +0000

    Update check_prime_number.py

--- prime_number/check_prime_number.py
+++ prime_number/check_prime_number.py
@@ -1,18 +1,17 @@
 def is_prime(num):
 
-        divisor = 2
+    divisor = 2
 
-            while divisor < num:
-                        if num % divisor == 0:
-                                        return False
-                                            divisor = divisor + 1
-                                                    
-                                                        return True
-                                                                
-                                                                
+    while divisor < num:
+        if num % divisor == 0:
+            return False
+        divisor = divisor + 1
+        
+    return True
+            
+            
 
-                                                    for i in range(1, 20):
-                                                            if is_prime(i + 1):
-                                                                            print(i + 1, end=" ")
-                                                                            print()
-
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
