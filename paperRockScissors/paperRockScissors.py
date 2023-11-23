4aeaff7c650a90cda92d5f467a2615a3435a58ff
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2023-11-23 15:17:23 +0000

    paperRockScissors game

--- paperRockScissors/paperRockScissors.py
+++ paperRockScissors/paperRockScissors.py
@@ -31,23 +31,24 @@ gameImage = [rock, paper, scissors]
 computer_choice = random.randint(0, 2)
 
 user_choice = int(input("Enter your choice: Type 0 for Rock, 1 for Paper, 2 for Scissors: "))
-print(f"User's choice is {gameImage[user_choice]} \nComputer's choice is {gameImage[computer_choice]}")
+
 
 if not (0 <= user_choice <= 2):
     print("Invalid number")
+else:
+    print(f"User's choice is {gameImage[user_choice]} \nComputer's choice is {gameImage[computer_choice]}")
 
-elif user_choice == computer_choice:
-    print("Draw")
+    if user_choice == computer_choice:
+        print("Draw")
 
-elif user_choice == 0 and computer_choice == 2:
-    print("Congratulations🎉, You won")
+    elif user_choice == 0 and computer_choice == 2:
+        print("Congratulations🎉, You won")
 
-elif user_choice == 2 and computer_choice == 0:
-    print("Opps😒, You lose")
+    elif user_choice == 2 and computer_choice == 0:
+        print("Opps😒, You lose")
 
-elif computer_choice > user_choice:
-    print("Opps😒, You lose")
+    elif computer_choice > user_choice:
+        print("Opps😒, You lose")
 
-elif user_choice > computer_choice:
-    print("Congratulations🎉, You won")
-    
+    elif user_choice > computer_choice:
+        print("Congratulations🎉, You won")
