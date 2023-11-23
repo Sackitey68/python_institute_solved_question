1bafadf711c5905c0b73e544ce0fcfced8ee34a1
Isaac Sackey Sackitey <sackitey68@yahoo.com>
2023-11-23 15:01:31 +0000

    Paper rock and scissors game

--- paperRockScissors/paperRockScissors.py
+++ paperRockScissors/paperRockScissors.py
@@ -1,23 +1,53 @@
 import random
 
-computer_choce = random.randint(0, 2)
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+gameImage = [rock, paper, scissors]
+computer_choice = random.randint(0, 2)
+
 user_choice = int(input("Enter your choice: Type 0 for Rock, 1 for Paper, 2 for Scissors: "))
-print(f"User's choice is {user_choice} \nComputer's choice is {computer_choce}")
+print(f"User's choice is {gameImage[user_choice]} \nComputer's choice is {gameImage[computer_choice]}")
 
 if not (0 <= user_choice <= 2):
     print("Invalid number")
 
-elif user_choice == computer_choce:
+elif user_choice == computer_choice:
     print("Draw")
 
-elif user_choice == 0 and computer_choce == 2:
+elif user_choice == 0 and computer_choice == 2:
     print("Congratulations🎉, You won")
 
-elif user_choice == 2 and computer_choce == 0:
+elif user_choice == 2 and computer_choice == 0:
     print("Opps😒, You lose")
 
-elif computer_choce > user_choice:
+elif computer_choice > user_choice:
     print("Opps😒, You lose")
 
-elif user_choice > computer_choce:
+elif user_choice > computer_choice:
     print("Congratulations🎉, You won")
+    
